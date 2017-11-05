d4e0a39eb05fa8bc33e13e52ebf2a7ccf161dd7f
Ashraf Uddin <ashrafuddin765@gmail.com>
2017-11-05 07:43:31 +0600

    Class-26

---
+++ php/class25.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Php Practice</title>
+
+	<style>
+		.phpcoding{
+			width: 900px;
+			margin: 0 auto;
+			background: #ddd;
+			padding: 20px;
+			min-height: 600px;
+		}
+
+		.headeroption{
+			background: #444;
+			color: #fff;
+			text-align: center;
+			padding: 20px;
+		}
+		.headeroption h2{
+			margin: 0;
+		}
+		.maincontent{
+			min-height: 400px;
+		}
+	</style>
+</head>
+<body>
+	<div class="phpcoding">
+		<section class="headeroption">
+			<h2>Php fundamental training</h2>
+		</section>
+		<section class="maincontent">
+
+		<hr>
+		<h4>PHP SuperGlobals Variable[$GLOBALS & $SERVER]</h4>
+		<hr>
+			<pre>
+		
+<?php
+
+	$x = 15;
+	$y = 10;
+
+	function sum(){
+		$GLOBALS['z'] = $GLOBALS['x']+$GLOBALS['y'];
+	}
+
+	sum();
+	echo $z;
+
+	echo $_SERVER['REMOTE_ADDR'];
+	echo "</br>";
+	var_dump($_SERVER);
+
+?>
+			</pre>
+
+
+
+		</section>
+		<section class="headeroption">
+			<h2>Php fundamental training</h2>
+		</section>
+	</div>
+</body>
+</html>
