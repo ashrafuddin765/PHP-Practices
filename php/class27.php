d3f365e9903e36b2d18023cd29c8905f9c9dba06
Ashraf Uddin <ashrafuddin765@gmail.com>
2017-11-07 07:40:57 +0600

    class 27

---
+++ php/class27.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Php Practice</title>
+    <style>
+    .phpcoding {
+        width: 900px;
+        margin: 0 auto;
+        background: #ddd;
+        padding: 20px;
+        min-height: 600px;
+    }
+
+    .headeroption {
+        background: #444;
+        color: #fff;
+        text-align: center;
+        padding: 20px;
+    }
+
+    .headeroption h2 {
+        margin: 0;
+    }
+
+    .maincontent {
+        min-height: 400px;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="phpcoding">
+        <section class="headeroption">
+            <h2>Php fundamental training</h2>
+        </section>
+        <section class="maincontent">
+            <hr>
+            <h4>PHP SuperGlobals Variable[$_GET]</h4>
+            <hr>
+            
+            <a href="test.php?name=shahria&msg=good%20bye">send data</a>
+            
+            <?php
+
+			?>
+        </section>
+        <section class="headeroption">
+            <h2>Php fundamental training</h2>
+        </section>
+    </div>
+</body>
+
+</html>
---
+++ php/test.php
@@ -0,0 +1,9 @@
+<?php
+$name = $_GET['name'];
+$msg = $_GET['msg'];
+
+echo 'Name: '.$name;
+echo '</br>';
+
+echo 'SMS'.$msg;
+?>
