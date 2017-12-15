c1be775e92151f5b0f100846c1b33129d2f766b0
Ashraf Uddin <ashrafuddin765@gmail.com>
2017-12-15 07:20:03 +0600

    PHP storm

---
+++ php-oop/practice1.php
@@ -0,0 +1,42 @@
+<?php include('header.php'); ?>
+
+
+
+<?php
+	/**
+	* Class Practice
+	*/
+	class person {
+			public $name;
+			public $age;
+
+
+		public function personName(){
+
+		}
+
+		public function personAge(){
+
+		}
+
+
+
+
+	}
+
+
+	$personeOne = new person;
+?>
+   
+
+
+
+
+
+
+
+
+
+
+
+<?php include('footer.php'); ?>
